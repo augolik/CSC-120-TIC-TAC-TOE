8299635980a17d691d5e0d8943e11a4fe390623c
augolik <anyaugolik@gmail.com>
2021-12-11 21:29:56 -0500

    Board printing functionality added

---
+++ board.py
@@ -0,0 +1,10 @@
+#create a blank board
+board = [['-','-','-']
+        ,['-','-','-']
+        ,['-','-','-']]
+
+#create a function that prints the board
+def printBoard():
+        print(board[0])
+        print(board[1])
+        print(board[2])
